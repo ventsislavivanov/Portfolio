9689e627cfed51a3451653a310ff2bfbc8ad9b21
ventsislav.ivanov89@gmail.com <haideNarab0ta>
2017-06-26 23:55:20 +0300

    Add Project Snowboards

---
+++ snowboards/js/function.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+
+
+$('.bxslider').bxSlider({
+  mode: 'fade',
+  auto: true,
+  pause: 2800,
+  pager: false,
+  controls: false,
+});
