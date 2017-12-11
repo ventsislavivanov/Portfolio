8459a255a173a3cdfc9eff97421686b3f6e9bd3b
ventsislavivanov <ventsislav.ivanov89@gmail.com>
2017-12-11 23:20:20 +0200

    Add project LaBelle created by Bootstrap and SASS

---
+++ La Belle/js/functions.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+
+
+$(document).ready(function(){
+  $('.slider4').bxSlider({
+  	auto: true,
+    slideWidth: 350,
+    minSlides: 2,
+    maxSlides: 7,
+    moveSlides: 1,
+    slideMargin: 10,
+    pager: false,
+  });
+});
